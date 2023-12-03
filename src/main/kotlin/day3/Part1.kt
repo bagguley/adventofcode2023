e61c11199bc06f72df27efaa010e2b74101935e1
Wayne Bagguley <wayne.bagguley@gmail.com>
2023-12-03 10:22:31 +0000

    Day 3 - Part 2

--- src/main/kotlin/day3/Part1.kt
+++ src/main/kotlin/day3/Part1.kt
@@ -6,25 +6,55 @@ fun main() {
 
 fun load(input: List<String>) {
     val partData: MutableMap<Coordinates, DataPoint> = mutableMapOf()
-
+    var id = 0
     input.forEachIndexed { y, line ->
         line.forEachIndexed { x, character ->
-            partData[Coordinates(x, y)] = DataPoint(Coordinates(x, y), character)
+            partData[Coordinates(x, y)] = DataPoint(id++, Coordinates(x, y), character)
         }
     }
+
+    val gearData: MutableMap<Coordinates, DataPoint> = mutableMapOf()
 
     partData.forEach { (coordinates, dataPoint) ->
         if (dataPoint.isSymbol()) {
             val surrounding = coordinates.surrounding()
-
             surrounding.forEach {
-                val part = partData[it]!!
-                if (part.isDigit()) {
-                    part.isAdjacent = true
+                val surroundingPart = partData[it]!!
+                if (surroundingPart.isDigit()) {
+                    surroundingPart.isAdjacent = true
                 }
             }
         }
     }
+
+    for (y in 0..139) {
+        for (x in 0..139) {
+            val part = partData[Coordinates(x, y)]!!
+            if (part.isDigit()) {
+                val adjacentDigits = part.coordinates.adjacentLine()
+                adjacentDigits.forEach {
+                    val adjacentPart = partData[it]!!
+                    if (adjacentPart.isDigit()) {
+                        adjacentPart.id = part.id
+                    }
+                }
+            }
+        }
+    }
+
+    partData.forEach { (coordinates, dataPoint) ->
+        if (dataPoint.isGearSymbol()) {
+            gearData[coordinates] = dataPoint
+            val surrounding = coordinates.surrounding()
+            surrounding.forEach {
+                val surroundingPart = partData[it]!!
+                if (surroundingPart.isDigit()) {
+                    dataPoint.touchesGears.add(surroundingPart.id)
+                }
+            }
+        }
+    }
+
 
     var changed = true
 
@@ -58,17 +88,45 @@ fun load(input: List<String>) {
     }
 
     val numbers = stringBuilder.trim().split(Regex("\\s+"))
-    println(numbers.map { it.toInt() }.sum())
+    println(numbers.sumOf { it.toInt() })
+
+    val gears = gearData.filter { (_, value) ->
+        value.touchesGears.size == 2
+    }
+
+    val ratios = gears.map { (_, value) ->
+        value.touchesGears.map { idToNumber(it, partData) }.reduceRight{ a, b ->  a * b }
+    }.sum()
+
+    println(ratios)
+}
+
+fun idToNumber(id: Int, partData: Map<Coordinates, DataPoint>): Int {
+    val stringBuilder = StringBuilder()
+    for (y in 0..139) {
+        for (x in 0..139) {
+            val part = partData[Coordinates(x,y)]!!
+            if (part.id == id) {
+                stringBuilder.append(part.character)
+            }
+        }
+    }
+    return stringBuilder.toString().toInt()
 }
 
 
-data class DataPoint(val coordinates: Coordinates, val character: Char, var isAdjacent: Boolean = false) {
+data class DataPoint(var id: Int, val coordinates: Coordinates, val character: Char, var isAdjacent: Boolean = false,
+                     val touchesGears: MutableSet<Int> = mutableSetOf()) {
     fun isSymbol(): Boolean {
         return !character.isDigit() && character != '.'
     }
 
     fun isDigit(): Boolean {
         return character.isDigit()
+    }
+
+    fun isGearSymbol(): Boolean {
+        return character == '*'
     }
 }
 
